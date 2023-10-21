62653c313177440d10e219a3136d661852926304
charlesoller <charlesrello@gmail.com>
2023-10-21 12:22:25 -0400

    Thru problem 4 done

--- array-practice.js
+++ array-practice.js
@@ -1,38 +1,66 @@
 const findMinimum = arr => {
-
-  // Your code here 
-
+  let min = arr[0];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] < min){
+      min = arr[i]
+    }
+  }
+  return min;
 };
 
 const runningSum = arr => {
+  let sumArr = [];
+  let runningTotal = 0;
 
-  // Your code here 
+  for(let i = 0; i < arr.length; i++){
+    runningTotal += arr[i];
+    sumArr.push(runningTotal);
+  }
+
+  return sumArr;
 };
 
 const evenNumOfChars = arr => {
-
-  // Your code here 
+  let counter = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].length % 2 === 0){
+      counter++;
+    }
+  }
+  return counter;
 };
 
 const smallerThanCurr = arr => {
+  let smallerArr = [];
 
-  // Your code here 
+  for(let i = 0; i < arr.length; i++){
+    let max = arr[i];
+    let counter = 0;
+    for(let j = 0; j < arr.length; j++){
+      if(arr[j] < max){
+        counter++;
+      }
+    }
+    smallerArr.push(counter);
+  }
 
+  return smallerArr;
 };
+
 
 const twoSum = (arr, target) => {
 
-  // Your code here 
+  // Your code here
 };
 
 const secondLargest = arr => {
 
-  // Your code here 
+  // Your code here
 };
 
 const shuffle = (arr) => {
 
-  // Your code here 
+  // Your code here
 };
 
 
